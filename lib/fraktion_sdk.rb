02656b768b86eb91194c7e0a90747614f54663f7
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-21 15:21:32 +0200

    fix commit inject transfer

--- lib/fraktion_sdk.rb
+++ lib/fraktion_sdk.rb
@@ -7,6 +7,7 @@ require_relative 'fraktion_sdk/configuration.rb'
 require_relative 'fraktion_sdk/base.rb'
 require_relative 'fraktion_sdk/helpers.rb'
 require_relative 'fraktion_sdk/create_managed/create.rb'
+require_relative 'fraktion_sdk/inject/transfer/create.rb'
 
 
 module FraktionSdk
