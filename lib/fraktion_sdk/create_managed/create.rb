1df79102c4d138ef4d605d4657fba19e36baa05f
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-21 16:01:35 +0200

    update api

--- lib/fraktion_sdk/create_managed/create.rb
+++ lib/fraktion_sdk/create_managed/create.rb
@@ -5,11 +5,13 @@ module FraktionSdk
     class << self
       def create
         puts generate_uri
-        #FraktionSdk.authenticated do |access_token, token_type|
-        #  conn_client.get(generate_uri) do |req|
-          
-        #  end
-        #end
+        FraktionSdk.authenticated do |access_token, token_type|
+          #required_keys = %i[from txs]
+          #params = ensure_keys(hash, required_keys)
+          conn.post(generate_uri, params.to_json,"Content-Type" => "application/json") do |req|
+            req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
+          end
+        end
       end
     end
   end
