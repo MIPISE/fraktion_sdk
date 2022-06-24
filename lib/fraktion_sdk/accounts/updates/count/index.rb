935dc9693e99b185cd3214df023c3b7acf22225c
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-24 23:17:05 +0200

    add api

---
+++ lib/fraktion_sdk/accounts/updates/count/index.rb
@@ -0,0 +1,16 @@
+module FraktionSdk
+  module Accounts
+    module Updates
+      module Count
+        extend Helpers
+        def self.index(hash = {})
+          FraktionSdk.authenticated do |access_token, token_type|
+            conn.get(generate_uri, hash.to_json,"Content-Type" => "application/json") do |req|
+              req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
+            end
+          end
+        end
+      end
+    end
+  end
+end
