e785075d2485ffb3897bb4d904e6d217eb1c2d1b
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-24 22:13:19 +0200

    update

--- lib/fraktion_sdk/tokens/token_id/index.rb
+++ lib/fraktion_sdk/tokens/token_id/index.rb
@@ -8,13 +8,11 @@ module FraktionSdk
           params = ensure_keys(hash, required_keys)
           token_id = params[:token_id]
           params.delete(:token_id)
-          puts params
-          puts generate_uri.gsub("tokenid", token_id.to_s)
-          #FraktionSdk.authenticated do |access_token, token_type|
-          #  conn.get(generate_uri.gsub("tokenid", token_id.to_s), "Content-Type" => "application/json") do |req|
-          #    req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
-          #  end
-          #end
+          FraktionSdk.authenticated do |access_token, token_type|
+            conn.get(generate_uri.gsub("tokenid", token_id.to_s), "Content-Type" => "application/json") do |req|
+              req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
+            end
+          end
         end
       end
     end
