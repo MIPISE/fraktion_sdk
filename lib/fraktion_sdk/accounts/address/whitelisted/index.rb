5573ee439b08a8879dc6b0289ae6478501a520ec
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-25 13:45:42 +0200

    update

--- lib/fraktion_sdk/accounts/address/whitelisted/index.rb
+++ lib/fraktion_sdk/accounts/address/whitelisted/index.rb
@@ -10,7 +10,8 @@ module FraktionSdk
           params.delete(:address) || params.delete("address")
           puts address
           FraktionSdk.authenticated do |access_token, token_type|
-            conn.get(generate_uri, "Content-Type" => "application/json") do |req|
+            debugger
+            conn.get(generate_uri.gsub("address", address.to_s), "Content-Type" => "application/json") do |req|
               req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
             end
           end
