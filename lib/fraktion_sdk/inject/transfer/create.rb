080c7b4bdd3297c3ff94cdbfdedb3ba599a24101
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-25 16:35:33 +0200

    update unit test

--- lib/fraktion_sdk/inject/transfer/create.rb
+++ lib/fraktion_sdk/inject/transfer/create.rb
@@ -6,8 +6,7 @@ module FraktionSdk
         FraktionSdk.authenticated do |access_token, token_type|
           #required_keys = %i[from txs]
           #params = ensure_keys(hash, required_keys)
-          puts "nicolas"
-          conn.post(generate_uri, hash,"Content-Type" => "application/json") do |req|
+          conn.post(generate_uri, hash.to_json,"Content-Type" => "application/json") do |req|
             req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
           end
         end
