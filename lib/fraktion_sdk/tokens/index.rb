89124a5ed813b1e35bc56214882387cd043cae58
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-06-27 15:42:53 +0200

    update gem

--- lib/fraktion_sdk/tokens/index.rb
+++ lib/fraktion_sdk/tokens/index.rb
@@ -3,7 +3,8 @@ module FraktionSdk
     extend Helpers
     def self.index(hash = {})
       FraktionSdk.authenticated do |access_token, token_type|
-        conn.get(generate_uri, hash.to_json,"Content-Type" => "application/json") do |req|
+        hash = hash.to_json unless hash.empty?
+        conn.get(generate_uri, hash,"Content-Type" => "application/json") do |req|
           req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
         end
       end
--- test/inject/transfer/create_test.rb
+++ test/inject/transfer/create_test.rb
@@ -1,22 +1,36 @@
 describe "Inject transfer" do
   it "call api with get hash in response" do
-    params = [{"from":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n",
-               "txs":[{"to":"tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC","token_id":"3","amount":"10"},
-               {"to":"KT1Cfbuk3TuLthedbn4HsgKV5zT1YQtGpwfi","token_id":"3","amount":"13"}]},
-              {"from":"KT1BYZboA4yssjsiCC2QrHodfFjaTzh7NREp",
-               "txs":[{"to":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n","token_id":"1","amount":"1"},
-               {"to":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n","token_id":"4","amount":"1"}]}]
+    params =     
+    [
+      {
+        "from": "KT1ScS8wnxNiQiBNC9Pe5gPLpp6DmizFLFcV",
+        "txs": [
+          {
+            "to": "KT19f2HYzjoXkvUKwaJmtVNmE6uq4vdhy9Ws",
+            "token_id": "5",
+            "amount": "35"
+          }
+        ]
+      }
+    ]
     inject_transfer = FraktionSdk::Inject::Transfer.create(params)
     assert_equal true, inject_transfer.body.has_key?("hash")
   end
 
   it "call api with get id" do
-    params = [{"from":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n",
-               "txs":[{"to":"tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC","token_id":"3","amount":"10"},
-               {"to":"KT1Cfbuk3TuLthedbn4HsgKV5zT1YQtGpwfi","token_id":"3","amount":"13"}]},
-              {"from":"KT1BYZboA4yssjsiCC2QrHodfFjaTzh7NREp",
-               "txs":[{"to":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n","token_id":"1","amount":"1"},
-               {"to":"KT1QPQdEBFkmWCJkZwqvE1nLyax4Hu9qvT3n","token_id":"4","amount":"1"}]}]
+    params = 
+    [
+      {
+        "from": "KT1ScS8wnxNiQiBNC9Pe5gPLpp6DmizFLFcV",
+        "txs": [
+          {
+            "to": "KT19f2HYzjoXkvUKwaJmtVNmE6uq4vdhy9Ws",
+            "token_id": "5",
+            "amount": "35"
+          }
+        ]
+      }
+    ]
     inject_transfer = FraktionSdk::Inject::Transfer.create(params)
     assert_equal true, inject_transfer.body.has_key?("id")
   end
