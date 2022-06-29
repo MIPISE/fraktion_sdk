b0c011e91b215090441914294a675e26bf587abc
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2022-06-29 10:21:52 +0200

    try fix test

--- test/test_fraktion_sdk.rb
+++ test/test_fraktion_sdk.rb
@@ -4,6 +4,7 @@ require "test_helper"
 
 
 class TestFraktionSdk < Minitest::Test
+  require_relative "inject/mint/create_test.rb"
   require_relative "create_managed/create_test.rb"
   require_relative "inject/transfer/create_test.rb"
   require_relative "accounts/address/whitelisted/index_test.rb"
@@ -14,7 +15,6 @@ class TestFraktionSdk < Minitest::Test
   require_relative "injections/index_test.rb"
   require_relative "injections/count/index_test.rb"
   require_relative "injections/index_test.rb"
-  require_relative "inject/mint/create_test.rb"
   #require_relative "inject/create_test.rb"
   #require_relative "injections/injection_id/show_test.rb"
   #def test_that_it_has_a_version_number
