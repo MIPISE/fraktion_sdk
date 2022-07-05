ccca6f87c884d68c050a569e51ff4225385e7ba8
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-07-05 14:27:55 +0200

    add unit test

--- test/inject/mint/create_test.rb
+++ test/inject/mint/create_test.rb
@@ -13,4 +13,20 @@ describe "Mint : Mint a token" do
     injection = FraktionSdk::Inject::Mint.create(params)
     assert_equal 200, injection.status
   end
+
+  it "call api when create token_id" do
+    params = {
+      "owners": [
+        {
+          "address": "tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC",
+          "amount": "1"
+        },
+      ],
+      "info": { "metadfcsdfata": { "name": "blockchain_3" } },
+      "options": {}
+    }
+
+    injection = FraktionSdk::Inject::Mint.create(params)
+    assert_equal 200, injection.status
+  end
 end
