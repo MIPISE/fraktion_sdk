53d73cd8a617ad79fbb4874dacfdc3658b89bbf9
VANDENBOGAERDE Nicolas <nicolas.vandenbogaerde@gmail.com>
2022-07-06 15:24:26 +0200

    update

--- lib/fraktion_sdk/inject/mint/create.rb
+++ lib/fraktion_sdk/inject/mint/create.rb
@@ -4,9 +4,9 @@ module FraktionSdk
       extend Helpers
       def self.create(hash = {})
         FraktionSdk.authenticated do |access_token, token_type|
-          required_keys = %i[owners info]
-          params = ensure_keys(hash, required_keys)
-          conn.post(generate_uri, params.to_json, "Content-Type" => "application/json") do |req|
+          #required_keys = %i[owners info]
+          #params = ensure_keys(hash, required_keys)
+          conn.post(generate_uri, hash.to_json, "Content-Type" => "application/json") do |req|
             req.headers['Authorization'] = "#{token_type} #{access_token}" if access_token && token_type
           end
         end
